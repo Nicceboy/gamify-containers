07510cf4be2c0f343413e4496a4c3893773743e4
Niklas Saari <niklas.saari@tutanota.com>
2021-02-23 00:30:04 +0200

    Refactoring, add more optional arguments

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # Gamifying containers - play your Windows games on Linux containers with Lutris and Wine
 
-This project essentially attempts to set suitable environment for running Windows games from Linux containers. Python script is used to run specifically made container.
+This project essentially attempts to set suitable environment for running Windows games from Linux containers. Python script is used to run specifically made container maybe with easier way. Docker has been used as container engine.
 
 Game installation and management is handled with [Lutris](https://lutris.net/) which attempts to automate a lot of painful stuff, such as managing suitable Wine versions, required dependencies for game to be functional and handling runner environments such as [dxvk](https://github.com/doitsujin/dxvk) for DirectX to Vulkan transitions for performance boost.
+
+In the end, provided Python script only opens the graphical user interface of the Lutris from the container.
+
+Game data, Wine prefix and Lutris environment is stored in volume.
+Runtime Linux is defined in Docker image. See Dockerfile for [reference.](Dockerfile)
 
 ## Why the hassle
 
   * Setting up your specific GPU drivers with Vulkan support, Wine and other mandatory software comes with a lot of dependencies, and you *might not always want* to get those in your host system
-  * Isolated code execution, at least on some level. Exposed components are listed on [exposed volumes](#exposed-components-from-the-host) section.
+  * Isolated code execution, at least on some level. Exposed components are listed on [exposed components](#exposed-components-from-the-host) section.
   * Easier version upgrade on those packages
   * If you need some space, it is easy to just delete Docker image(s) but maintain the game data itself from volumes.
+
+This has probably downside for performance, but sometimes it is not that meaningful.
 
 ## Prerequisites
   * Docker Engine
@@ -25,13 +32,14 @@ If you don't have Xorg display server already, you might want to skip this proje
 
 However, sometimes in the log-in phase for Linux distribution, you can select the environment, e.g. Gnome With Xorg if it is installed, but not used.
 
+
 ## Configuring sound
 
 This step is not automated, user should be aware of the changes.
 
 To be able to get sound from the container, Unix socket for PulseAudio must be created beforehand. This can be achieved by modifying configuration files and then restarting server.
 
-Server will enabled to be accessed by other users.
+Server will enabled to be accessed by other users who belong to `pulseaudio` group.
 Look for user-specific pulse configuration file in path:
 ```
 cat "${HOME}/.config/pulse/default.pa"
@@ -39,26 +47,29 @@ cat "${HOME}/.config/pulse/default.pa"
 Create/modify the file with following contents:
 ```ini
 .include /etc/pulse/default.pa
-load-module module-native-protocol-unix 
-auth-anonymous=1 
-socket=/tmp/pulse-socket
+load-module module-native-protocol-unix auth-group=pulseaudio socket=/tmp/pulse-socket
 ```
 
-Then restart server
+Default socket path fill be `/tmp/pulse-socket`.Then restart server.
 ```cmd
 pulseaudio -k
 pulseaudio --start
 pulseaudio --check
 ```
-Socket should be available 
+Socket should be available. 
 ```cmd
-file /tmp/pulse-socket
+$ file /tmp/pulse-socket
+/tmp/pulse-socket: socket
 ```
+
+## Graphic Cards
+
+By default, graphic cards are passed as devices into container from the path `/dev/dri`. Provided image has AMD and Intel drivers installed, with 32 and 64-bit Vulkan support. NVIDIA drivers should be installed manually, if there is need for them.
 
 
 ## Exposed components from the host
 
-Following paths are exposed with read-only binds from the ~~~~host system into container.
+Following paths are exposed with read-only binds from the host system into container.
 
  * Xorg server - `/tmp/.X11-unix`
  * PulseAudio socket - `/tmp/pulse-socket`
@@ -66,3 +77,11 @@ Following paths are exposed with read-only binds from the ~~~~host system into c
  Passed as devices
  * Sound cards - `/dev/snd`
  * Graphic cards - `/dev/dri`
+ 
+ ## Command line commands without using Python
+ 
+ TODO
+ 
+ ## License 
+ 
+ MIT
--- play.py
+++ play.py
@@ -11,13 +11,18 @@ import subprocess
 import tarfile
 from typing import Dict, List
 
-DEFAULT_IMAGE = "wine-remote"
+# Default values
+
+DEFAULT_IMAGE = "lutris-vulkan"
 VOLUME_NAME = "winehome"
 SHM_SIZE = "4g"
+PULSE_SOCKET = "/tmp/pulse-socket"
+X_SOCKET_DIR = "/tmp/.X11-unix"
+LUTRIS_RELATIVE_PATH = "lutris/bin/lutris"
 
 
 class ContainerRuntime:
-    def __init__(self, image: str, entrypoint: str = ""):
+    def __init__(self, image: str, pulse_path: str, x_path: str, entrypoint: str = ""):
         self.logger = logging.getLogger("config")
         try:
             self.client = docker.from_env(version="auto")
@@ -27,6 +32,8 @@ class ContainerRuntime:
         self.envs: Dict = {}
         self.volumes: Dict = {}
         self.devices: List = []
+        self.pulse_path = pathlib.Path(pulse_path)
+        self.x_path = pathlib.Path(x_path)
         self.main_volume = "/home/wineuser"
         self.define_volumes()
         self.pass_devices()
@@ -36,8 +43,9 @@ class ContainerRuntime:
         except docker.errors.NotFound:
             self.logger.error("Image pull not implemented yet.")
             sys.exit(1)
+        # Container starts Lutris on debug mode
         self.container = self.client.containers.create(image=self.image, auto_remove=True,
-                                                       command=[f"{self.main_volume}/lutris/bin/lutris", "-d"],
+                                                       command=[f"{self.main_volume}/{LUTRIS_RELATIVE_PATH}", "-d"],
                                                        devices=self.devices,
                                                        environment=self.envs, shm_size=SHM_SIZE,
                                                        volumes=self.volumes)
@@ -48,19 +56,18 @@ class ContainerRuntime:
     def define_volumes(self):
         # --volume="${USER_VOLUME}:${USER_HOME}"
         # Pulseaudio server socket
-        pulse_path = pathlib.Path("/tmp/pulse-socket")
-        if pulse_path.is_socket():
-            self.volumes[str(pulse_path)] = {"bind": str(pulse_path), "mode": "ro"}
-            self.logger.debug(f"Pulseaudio found in path: {pulse_path}")
+        if self.pulse_path.is_socket():
+            self.volumes[str(self.pulse_path)] = {"bind": str(self.pulse_path), "mode": "ro"}
+            self.logger.debug(f"Pulseaudio found from in the path: {self.pulse_path}")
         else:
-            self.logger.warning(f"Socket for Pulseaudio from the path '{pulse_path}' not found. Sounds will not work.")
-        # Xorg socket, read-only
-        x_path = pathlib.Path("/tmp/.X11-unix")
-        if x_path.is_dir():
-            self.volumes[str(x_path)] = {"bind": str(x_path), "mode": "ro"}
-            self.logger.debug(f"X server found in the path: {x_path}")
+            self.logger.warning(f"Socket for Pulseaudio from the path '{self.pulse_path}'"
+                                " not found. Sounds will not work.")
+        # Xorg directory, read-only
+        if self.x_path.is_dir():
+            self.volumes[str(self.x_path)] = {"bind": str(self.x_path), "mode": "ro"}
+            self.logger.debug(f"X server found in the path: {self.x_path}")
         else:
-            self.logger.error(f"X server not found from the path '{x_path}'. Exiting...")
+            self.logger.error(f"X server not found from the path '{self.x_path}'. Exiting...")
             sys.exit(1)
 
         # Volume for wine prefix or home directory
@@ -72,7 +79,7 @@ class ContainerRuntime:
             self.logger.error("Creation not implemented yet..exiting..")
             sys.exit(1)
 
-        self.logger.info("Volumes defined. Following paths exposed in read-only:")
+        self.logger.info("Following volume(s) exposed from the host:")
         for key in self.volumes.keys():
             self.logger.info(key)
 
@@ -128,23 +135,39 @@ class ContainerRuntime:
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-l", "--log", dest="log_level", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
-                        help="Set the logging level", default=None)
+                        help="Set the logging level", default="INFO")
+    parser.add_argument("-d", "--detach", action="store_true",
+                        help="Run detached.")
+    parser.add_argument("--pulse", help="Set path for PulseAudio socket.", default=PULSE_SOCKET)
+    parser.add_argument("--xorg", help="Set path for X socket directory.", default=X_SOCKET_DIR)
     args = parser.parse_args(args=sys.argv[1:])
     log_level = args.log_level if args.log_level else 'INFO'
     if log_level not in {'DEBUG'}:
         sys.tracebacklimit = 0  # track traces only when debugging
     logging.basicConfig(format='%(name)s: %(message)s', level=getattr(logging, log_level))
-
-    runtime = ContainerRuntime(DEFAULT_IMAGE)
-    runtime.container.start()
-    for data in runtime.socket:
-        # No use for header yet
-        # header = data[:8]
-        body = data[8:]
-        print(body.decode("utf-8"), end="")
-        # print("NEWLINE")
+    logger = logging.getLogger("main")
+    runtime = ContainerRuntime(DEFAULT_IMAGE, args.pulse, args.xorg)
+    try:
+        logger.info(f"Starting Lutris in container with id {runtime.container.short_id}")
+        runtime.container.start()
+        if not args.detach:
+            for data in runtime.socket:
+                # No use for header yet
+                # header = data[:8]
+                body = data[8:]
+                print(body.decode("utf-8"), end="")
+                # print("NEWLINE")
+        else:
+            logger.info("Leaving and not printing Lutris logs in detached mode.")
+    finally:
+        if not args.detach:
+            logger.info("Killing container...")
+            try:
+                runtime.container.kill()
+            except docker.errors.APIError:
+                logger.debug("Could not kill container...not running anymore.")
 
 
 def parse_data():
